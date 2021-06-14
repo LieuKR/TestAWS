481016fb473a3e74c53f29dddb4f7d4dde408ce0
Lieu <shck1010@naver.com>
2021-06-14 18:08:09 +0900

    testingasgag

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/post', function(req, res, next) {
+  console.log('testing for post')
+  console.log(req.body.id)
+  res.redirect('back');
 
+  let random_value = Math.floor(Math.random()*10000)
+  console.log(random_value);
+  
+
+
+  var exec = require('child_process').exec,
+    child;
+
+  child = exec(`sudo useradd -g sftponly-d /incoming -s /sbin/nologin ${req.body.id}
+  `, function (error, stdout, stderr) {
+    console.log('testing1111111111111111111')
+
+  });
+});
 
 
 
