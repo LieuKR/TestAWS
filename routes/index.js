2e14cc26bea2c47509e7769319b02e01e0f15e45
Lieu <shck1010@naver.com>
2021-06-15 12:54:23 +0900

    test9

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const {spawn} = require('child_process');
-
-
+var spawn = require('child_process').spawn
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,60 +10,46 @@ router.get('/', function(req, res) {
 
 router.post('/post', function(req, res) {
   console.log('Writed id is ', req.body.id)
-
-  const child = spawn(`sudo useradd -G sftponly ${req.body.id}`);
-
-  // this value is NOT SAFE as random value
-  let random_value = Math.floor(Math.random()*10000)
-  console.log(random_value);
-
-  child.on('exit', function (code, signal) {
-    console.log('child process exited with ' +
-                `code ${code} and signal ${signal}`);
-  });
   
   // Create user for linux.
-  // spawn(`sudo useradd -G sftponly ${req.body.id}`, function () {
+  spawn(`sudo useradd ${req.body.id}`, function (error, stdout, stderr) {
 
-  // child.on(`sudo passwd ${req.body.id}`, function () {
-  //   console.log(`userid ${req.body.id} is created.`)
-  //   console.log(`now need to make pw`)
-  // })
-  
+    console.log(`userid ${req.body.id} is created.`)
 
-    // // this value is NOT SAFE as random value
-    // let random_value = Math.floor(Math.random()*10000)
-    // console.log(random_value);
+    // this value is NOT SAFE as random value
+    let random_value = Math.floor(Math.random()*10000)
+    console.log(random_value);
 
-    // to set password by random number
-    // const child = spawn(`sudo passwd ${req.body.id}`);
-
-    // child.on('exit', function () {
-    //   console.log('child.on is working');
-    //   });
-
-    // });
-
-
-/*
     // 1 More callback to set password by random number
-    spawn(`sudo passwd ${req.body.id}`, function () {
+    spawn(`sudo passwd ${req.body.id}`, function (error, stdout, stderr) {
       
 
 
-      
-      // write password
-      spawn(`${random_value}`, function () {
-      
-        // repeat password
-        spawn(`${random_value}`, function () {
-      
-          console.log('This is password', req.body.id)
 
-        });
-      });
+
+
+      
+      // // write password
+      // spawn(`${random_value}`, function () {
+      
+      //   // repeat password
+      //   spawn(`${random_value}`, function () {
+      
+      //     console.log('This is password', req.body.id)
+
+      //   });
+      // });
+
+
+
+
+
+
+
+
+
     });
-*/
+  });
 
   res.redirect('back');
 
@@ -76,30 +60,28 @@ router.post('/post', function(req, res) {
 
 module.exports = router;
 
-
 /*
 const spawn = require('child_process').spawn;
+const ffmpeg = spawn('ffmpeg', ['-y', '-i', `${origin}`, '-acodec', 'aac', '-ab', '192k', '-ar', '48000', '-ac', '2', '-b:a', '300k', '-vcodec', 'libx264', '-level', '30', '-b:v', '500k', '-r', '23', '-vf', 'scale=\'min(320,iw)\':\'min(240,ih)\'', '-threads', '0', '-strict', '-2', '-movflags', 'faststart', `${dest}`]);
 
-  const ffmpeg = spawn('ffmpeg', ['-y', '-i', `${origin}`, '-acodec', 'aac', '-ab', '192k', '-ar', '48000', '-ac', '2', '-b:a', '300k', '-vcodec', 'libx264', '-level', '30', '-b:v', '500k', '-r', '23', '-vf', 'scale=\'min(320,iw)\':\'min(240,ih)\'', '-threads', '0', '-strict', '-2', '-movflags', 'faststart', `${dest}`]);
+var scriptOutput = '';
 
-  var scriptOutput = '';
+ffmpeg.stdout.setEncoding('utf8');
+ffmpeg.stdout.on('data', function(data) {
+  data = data.toString();
+  scriptOutput += data;
+});
 
-  ffmpeg.stdout.setEncoding('utf8');
-  ffmpeg.stdout.on('data', function(data) {
-    data = data.toString();
-    scriptOutput += data;
-  });
+ffmpeg.stderr.setEncoding('utf8');
+ffmpeg.stderr.on('data', function(data) {
+  //console.log('stderr AAsbn83N: ' + data);
+  data=data.toString();
+  scriptOutput += data;
+});
 
-  ffmpeg.stderr.setEncoding('utf8');
-  ffmpeg.stderr.on('data', function(data) {
-    //console.log('stderr AAsbn83N: ' + data);
-    data=data.toString();
-    scriptOutput += data;
-  });
-
-  ffmpeg.on('close', function(code) {
-    //console.log('close process VIDEO 8734b: ' + code);
-    return resolve({error:false, message:scriptOutput, code:code});
-  });
+ffmpeg.on('close', function(code) {
+  //console.log('close process VIDEO 8734b: ' + code);
+  return resolve({error:false, message:scriptOutput, code:code});
+});
 
 */
---
+++ routes/index_test.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var router = express.Router();
+
+const {spawn} = require('child_process');
+
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/post', function(req, res) {
+  console.log('Writed id is ', req.body.id)
+
+  const child = spawn(`sudo useradd -G sftponly ${req.body.id}`);
+
+  // this value is NOT SAFE as random value
+  let random_value = Math.floor(Math.random()*10000)
+  console.log(random_value);
+
+  child.on('exit', function (code, signal) {
+    console.log('child process exited with ' +
+                `code ${code} and signal ${signal}`);
+  });
+  
+  // Create user for linux.
+  // spawn(`sudo useradd -G sftponly ${req.body.id}`, function () {
+
+  // child.on(`sudo passwd ${req.body.id}`, function () {
+  //   console.log(`userid ${req.body.id} is created.`)
+  //   console.log(`now need to make pw`)
+  // })
+  
+
+    // // this value is NOT SAFE as random value
+    // let random_value = Math.floor(Math.random()*10000)
+    // console.log(random_value);
+
+    // to set password by random number
+    // const child = spawn(`sudo passwd ${req.body.id}`);
+
+    // child.on('exit', function () {
+    //   console.log('child.on is working');
+    //   });
+
+    // });
+
+
+/*
+    // 1 More callback to set password by random number
+    spawn(`sudo passwd ${req.body.id}`, function () {
+      
+
+
+      
+      // write password
+      spawn(`${random_value}`, function () {
+      
+        // repeat password
+        spawn(`${random_value}`, function () {
+      
+          console.log('This is password', req.body.id)
+
+        });
+      });
+    });
+*/
+
+  res.redirect('back');
+
+});
+
+
+
+
+module.exports = router;
+
+
+/*
+const spawn = require('child_process').spawn;
+
+  const ffmpeg = spawn('ffmpeg', ['-y', '-i', `${origin}`, '-acodec', 'aac', '-ab', '192k', '-ar', '48000', '-ac', '2', '-b:a', '300k', '-vcodec', 'libx264', '-level', '30', '-b:v', '500k', '-r', '23', '-vf', 'scale=\'min(320,iw)\':\'min(240,ih)\'', '-threads', '0', '-strict', '-2', '-movflags', 'faststart', `${dest}`]);
+
+  var scriptOutput = '';
+
+  ffmpeg.stdout.setEncoding('utf8');
+  ffmpeg.stdout.on('data', function(data) {
+    data = data.toString();
+    scriptOutput += data;
+  });
+
+  ffmpeg.stderr.setEncoding('utf8');
+  ffmpeg.stderr.on('data', function(data) {
+    //console.log('stderr AAsbn83N: ' + data);
+    data=data.toString();
+    scriptOutput += data;
+  });
+
+  ffmpeg.on('close', function(code) {
+    //console.log('close process VIDEO 8734b: ' + code);
+    return resolve({error:false, message:scriptOutput, code:code});
+  });
+
+*/
