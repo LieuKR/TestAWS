a63f3b196ef85008d7c1392f91b25f230f7fec56
Lieu <shck1010@naver.com>
2021-06-15 12:29:17 +0900

    test2

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const spawn = require('child_process').spawn;
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/post', function(req, res, next) {
-  console.log('testing for post')
-  console.log(req.body.id)
-  res.redirect('back');
-
-
+router.post('/post', function(req, res) {
+  console.log('Writed id is ', req.body.id)
   
-
-
-  var exec = require('child_process').exec
-
   // Create user for linux.
-  exec(`sudo useradd ${req.body.id}`, function () {
+  spawn(`sudo useradd -G sftponly ${req.body.id}`, function () {
 
     console.log(`userid ${req.body.id} is created.`)
 
@@ -26,23 +20,41 @@ router.post('/post', function(req, res, next) {
     let random_value = Math.floor(Math.random()*10000)
     console.log(random_value);
 
+    // to set password by random number
+    const child = spawn(`sudo passwd ${req.body.id}`);
+
+    child.on('exit', function () {
+      console.log('child.on is working');
+
+      //child.stdout
+
+
+    });
+
+
+/*
     // 1 More callback to set password by random number
-    exec(`sudo passwd ${req.body.id}`, function () {
+    spawn(`sudo passwd ${req.body.id}`, function () {
+      
+
+
       
       // write password
-      exec(`${random_value}`, function () {
+      spawn(`${random_value}`, function () {
       
         // repeat password
-        exec(`${random_value}`, function () {
+        spawn(`${random_value}`, function () {
       
           console.log('This is password', req.body.id)
 
         });
       });
     });
+*/
+
   });
 
-
+  res.redirect('back');
 
 });
 
@@ -50,3 +62,31 @@ router.post('/post', function(req, res, next) {
 
 
 module.exports = router;
+
+
+/*
+const spawn = require('child_process').spawn;
+
+  const ffmpeg = spawn('ffmpeg', ['-y', '-i', `${origin}`, '-acodec', 'aac', '-ab', '192k', '-ar', '48000', '-ac', '2', '-b:a', '300k', '-vcodec', 'libx264', '-level', '30', '-b:v', '500k', '-r', '23', '-vf', 'scale=\'min(320,iw)\':\'min(240,ih)\'', '-threads', '0', '-strict', '-2', '-movflags', 'faststart', `${dest}`]);
+
+  var scriptOutput = '';
+
+  ffmpeg.stdout.setEncoding('utf8');
+  ffmpeg.stdout.on('data', function(data) {
+    data = data.toString();
+    scriptOutput += data;
+  });
+
+  ffmpeg.stderr.setEncoding('utf8');
+  ffmpeg.stderr.on('data', function(data) {
+    //console.log('stderr AAsbn83N: ' + data);
+    data=data.toString();
+    scriptOutput += data;
+  });
+
+  ffmpeg.on('close', function(code) {
+    //console.log('close process VIDEO 8734b: ' + code);
+    return resolve({error:false, message:scriptOutput, code:code});
+  });
+
+*/
